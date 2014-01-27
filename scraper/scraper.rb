ff5d19f1bb0880820c04b87ceba2a6284a23db03
Caleb Everett <everett.caleb@gmail.com>
2014-01-26 21:24:10 -0500

    getting peers from html endpoints

--- scraper/scraper.rb
+++ scraper/scraper.rb
@@ -13,7 +13,7 @@ end
 class User
   attr_accessor :id, :name
 
-  def initialize id, name
+  def initialize id, name=nil
     @id, @name = id, name
   end
 
@@ -27,6 +27,28 @@ class User
     "https://github.com/#{id}?tab=activity"
   end
 
+  # Returns the address of a users following page.
+  def following_url
+    "https://github.com/#{name}/following"
+  end
+
+  # Returns the address of a users followers page.
+  def followers_url
+    "https://github.com/#{name}/followers"
+  end
+
+  def combined_followees
+    following | followers
+  end
+
+  def following
+    get_users following_url
+  end
+
+  def followers
+    get_users followers_url
+  end
+
   def to_s
     "#{name}: #{id}"
   end
@@ -38,44 +60,24 @@ class User
   def eql? other
     other.id == self.id
   end
-end
 
-# Returns the address of a users following page.
-def following_url username
-  "https://github.com/#{username}/following"
-end
+  private
 
-# Returns the address of a users followers page.
-def followers_url username
-  "https://github.com/#{username}/followers"
-end
+  def get_users url
+    Nokogiri::HTML(open(url)).css('li.follow-list-item').map do |elem|
+      # Get the account id from the image link.
+      id = elem.css('> a').first[:href][1..-1]
+      # Get the account name from the image alt text (github doesn't use great html markup)
+      name = elem.css('> a > img').first[:alt]
+      name = nil if name.blank?
 
-
-def get_combined_followees username
-  get_following(username) | get_followers(username)
-end
-
-def get_following username
-  get_users following_url username
-end
-
-def get_followers username
-  get_users followers_url username
-end
-
-def get_users url
-  Nokogiri::HTML(open(url)).css('li.follow-list-item').map do |elem|
-    # Get the account id from the image link.
-    id = elem.css('> a').first[:href][1..-1]
-    # Get the account name from the image alt text (github doesn't use great html markup)
-    name = elem.css('> a > img').first[:alt]
-    name = nil if name.blank?
-
-    User.new id, name
+      User.new id, name
+    end
   end
 end
 
-username = 'everett1992'
+
+user = User.new 'everett1992'
 
 puts '-- combined'
-puts get_combined_followees(username)
+puts user.combined_followees
