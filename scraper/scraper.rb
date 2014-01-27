1959879845312e844db35da06b806f25e75207c9
Caleb Everett <everett.caleb@gmail.com>
2014-01-27 13:50:07 -0500

    Adding events

--- scraper/scraper.rb
+++ scraper/scraper.rb
@@ -1,5 +1,6 @@
 #!/bin/env ruby
 
+require 'active_support/core_ext/numeric/time'
 require 'json'
 require 'open-uri'
 
@@ -10,52 +11,54 @@ class String
   end
 end
 
-# Authenticated interaction with github.
+# Authenticated curl from github.
 def github url
   open url, http_basic_authentication: [ENV['GITHUB_OAUTH_KEY'], '']
 end
 
 class User
   attr_accessor :login, :name
+  attr_reader :user_url, :events_url, :following_url, :followers_url
 
   def initialize login
     @login = login
+
+    #:: Defined API endpoints
+    @user_url = "https://api.github.com/users/#{login}"
+    @events_url = "https://api.github.com/users/#{login}/events/public"
+    @following_url = "https://api.github.com/users/#{login}/following"
+    @followers_url = "https://api.github.com/users/#{login}/followers"
   end
 
-  def user_url
-    "https://api.github.com/users/#{login}"
-  end
 
-  def events_url
-    "https://api.github.com/users/#{login}/events/public"
-  end
-
-  def following_url
-    "https://api.github.com/users/#{login}/following"
-  end
-
-  def followers_url
-    "https://api.github.com/users/#{login}/followers"
-  end
-
+  # The union of following, and followers
   def peers
-    following | followers
+    [:following, :followers].map do |m|
+      Thread.new { self.method(m).call }
+    end.map(&:value).flatten.uniq
   end
 
+  # Array of following Users
   def following
     get_users following_url
   end
 
+  # Array of follower Users
   def followers
     get_users followers_url
+  end
+
+  def events
+    JSON.parse(github(events_url).read).map { |event| Event.new event }
   end
 
   def to_s
     login
   end
 
+  # Users are uniquely identified by their login.
   def hash
-    self.login.hash
+    login.hash
   end
 
   def eql? other
@@ -76,10 +79,24 @@ class User
   end
 end
 
+class Event
+  attr_accessor :type, :repo_name, :created_at
+
+  def initialize hash
+    @type = hash['type']
+    @repo_name = hash['repo']['name']
+    @created_at = DateTime.parse(hash['created_at'])
+  end
+
+  def to_s
+    "#{created_at} - #{type}: #{repo_name}"
+  end
+end
+
 
 user = User.new 'everett1992'
 
 puts user.name
 
 puts '-- peers'
-puts user.peers
+puts user.events
