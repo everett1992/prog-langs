04fa0d96e223e32a8a3f12d1b1215e53f27f286e
Caleb Everett <everett.caleb@gmail.com>
2014-02-21 16:24:44 -0500

    Add more examples.

--- sudoku/prototype.rb
+++ sudoku/prototype.rb
@@ -3,31 +3,66 @@ require './sudoku'
 
 # Test puzzle, I don't feel like writing an input
 # function yet so it's hardcoded.
-ss = [
-  [7,   9,   nil, nil, nil, nil, 3,   nil, nil],
-  [nil, nil, nil, nil, nil, 6,   9,   nil, nil],
-  [8,   nil, nil, nil, 3,   nil, nil, 7,   6  ],
-  [nil, nil, nil, nil, nil, 5,   nil, nil, 2  ],
-  [nil, nil, 5,   4,   1,   8,   7,   nil, nil],
-  [4,   nil, nil, 7,   nil, nil, nil, nil, nil],
-  [6,   1,   nil, nil, 9,   nil, nil, nil, 8  ],
-  [nil, nil, 2,   3,   nil, nil, nil, nil, nil],
-  [nil, nil, 9,   nil, nil, nil, nil, 5,   4  ],
+pzls = [
+  [
+    [7,   9,   nil, nil, nil, nil, 3,   nil, nil],
+    [nil, nil, nil, nil, nil, 6,   9,   nil, nil],
+    [8,   nil, nil, nil, 3,   nil, nil, 7,   6  ],
+    [nil, nil, nil, nil, nil, 5,   nil, nil, 2  ],
+    [nil, nil, 5,   4,   1,   8,   7,   nil, nil],
+    [4,   nil, nil, 7,   nil, nil, nil, nil, nil],
+    [6,   1,   nil, nil, 9,   nil, nil, nil, 8  ],
+    [nil, nil, 2,   3,   nil, nil, nil, nil, nil],
+    [nil, nil, 9,   nil, nil, nil, nil, 5,   4  ]
+  ],
+  [
+    [nil, 1,   nil, nil, nil, 3,   nil, 5,   nil],
+    [9,   nil, nil, nil, nil, 2,   nil, nil, 1  ],
+    [nil, nil, 3,   9,   nil, 6,   nil, nil, 8  ],
+    [nil, nil, 5,   nil, 2,   nil, 9,   1,   nil],
+    [nil, nil, nil, nil, nil, nil, nil, nil, nil],
+    [nil, 8,   2,   nil, 9,   nil, 5,   nil, nil],
+    [1,   nil, nil, 3,   nil, 7,   8,   nil, nil],
+    [8,   nil, nil, 2,   nil, nil, nil, nil, 4  ],
+    [nil, 6,   nil, 8,   nil, nil, nil, 9,   nil]
+  ],
+  [
+    [8,   nil, nil, nil, nil, 4,   2,   nil, nil],
+    [3,   nil, nil, nil, 5,   nil, nil, 6,   nil],
+    [5,   nil, nil, nil, 3,   2,   nil, nil, nil],
+    [nil, nil, nil, nil, nil, nil, nil, 4,   2, ],
+    [nil, 2,   1,   nil, nil, nil, 3,   8,   nil],
+    [4,   7,   nil, nil, nil, nil, nil, nil, nil],
+    [nil, nil, nil, 3,   9,   nil, nil, nil, 6  ],
+    [nil, 8,   nil, nil, 7,   nil, nil, nil, 5  ],
+    [nil, nil, 6,   5,   nil, nil, nil, nil, 9  ]
+  ],
+  [
+    [8,   5,   nil, nil, nil, 2,   4,   nil, nil],
+    [7,   2,   nil, nil, nil, nil, nil, nil, 9  ],
+    [nil, nil, 4,   nil, nil, nil, nil, nil, nil],
+    [nil, nil, nil, 1,   nil, 7,   nil, nil, 2  ],
+    [3,   nil, 5,   nil, nil, nil, 9,   nil, nil],
+    [nil, 4,   nil, nil, nil, nil, nil, nil, nil],
+    [nil, nil, nil, nil, 8,   nil, nil, 7,   nil],
+    [nil, 1,   7,   nil, nil, nil, nil, nil, nil],
+    [nil, nil, nil, nil, 3,   6,   nil, 4,   nil]
+  ],
+  [
+    [nil, nil, 5, 3, nil, nil, nil, nil, nil],
+    [8, nil, nil, nil, nil, nil, nil, 2, nil],
+    [nil, 7, nil, nil, 1, nil, 5, nil, nil  ],
+    [4, nil, nil, nil, nil, 5, 3, nil, nil  ],
+    [nil, 1, nil, nil, 7, nil, nil, nil, 6  ],
+    [nil, nil, 3, 2, nil, nil, nil, 8, nil  ],
+    [nil, 6, nil, 5, nil, nil, nil, nil, 9  ],
+    [nil, nil, 4, nil, nil, nil, nil, 3, nil],
+    [nil, nil, nil, nil, nil, 9, 7, nil, nil],
+  ]
 ]
 
-hs = [
-  [nil, 1,   nil, nil, nil, 3,   nil, 5,   nil],
-  [9,   nil, nil, nil, nil, 2,   nil, nil, 1  ],
-  [nil, nil, 3,   9,   nil, 6,   nil, nil, 8  ],
-  [nil, nil, 5,   nil, 2,   nil, 9,   1,   nil],
-  [nil, nil, nil, nil, nil, nil, nil, nil, nil],
-  [nil, 8,   2,   nil, 9,   nil, 5,   nil, nil],
-  [1,   nil, nil, 3,   nil, 7,   8,   nil, nil],
-  [8,   nil, nil, 2,   nil, nil, nil, nil, 4  ],
-  [nil, 6,   nil, 8,   nil, nil, nil, 9,   nil]
-]
 
-[ss, hs].each do |pz|
+pzls.each do |pz|
   puts "-- puzzle"
   sudoku = Sudoku.new(pz)
   puts sudoku
