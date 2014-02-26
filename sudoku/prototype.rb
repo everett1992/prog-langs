8572a2f3753bb7dcde88eb46a0dad4d92e4eaacb
Caleb Everett <everett.caleb@gmail.com>
2014-02-26 01:48:49 -0500

    Added working 16x16

--- sudoku/prototype.rb
+++ sudoku/prototype.rb
@@ -60,32 +60,48 @@ pzls = [
     nil, nil, 4, nil, nil, nil, nil, 3, nil,
     nil, nil, nil, nil, nil, 9, 7, nil, nil,
   ],
-
   [
-    nil, nil, 'C', nil, 9,   'B', nil, nil, nil, nil, nil, nil, nil, nil, nil, nil,
-    nil, 2,   nil, nil, nil, 'D', 'G', nil, 7,   'B', 6,   nil, nil, 1,   nil, 'C',
-    nil, 1,   nil, 3,   nil, 'A', nil, 2,   'G', nil, nil, nil, nil, nil, nil, nil,
-    nil, nil, nil, 'D', 1,   3,   'F', nil, 'A', nil, nil, nil, nil, nil, nil, nil,
-    5,   6,   nil, nil, 3,   nil, nil, 9,   'F', nil, nil, nil, 4,   nil, nil, nil,
-    'B', nil, 9,   4,   nil, nil, 7,   'C', nil, 2,   nil, nil, nil, 'A', 1,   nil,
-    'D', nil, nil, nil, 'G', nil, 1,   nil, 5,   nil, nil, 8,   nil, nil, nil, 'E',
-    'E', nil, 'F', 2,   nil, nil, 5,   'A', nil, nil, nil, 'G', 'B', nil, nil, nil,
-    nil, nil, nil, 8,   'B', nil, nil, nil, 3,   4,   nil, nil, 'A', 'C', nil, 'G',
-    'G', nil, nil, nil, 'E', nil, nil, 5,   nil, 9,   nil, 'A', nil, nil, nil, 7,
-    nil, 4,   3,   nil, nil, nil, 9,   nil, 'E', 'D', nil, nil, 'F', 2,   nil, 'B',
-    nil, nil, nil, 'E', nil, nil, nil, 3,   8,   nil, nil, 'F', nil, nil, 6,   5,
-    nil, nil, nil, nil, nil, nil, nil, 'G', nil, 5,   2,   'E', 6,   nil, nil, nil,
-    nil, nil, nil, nil, nil, nil, nil, 'F', 4,   nil, 'G', nil, 'D', nil, 2,   nil,
-    'F', nil, 2,   nil, nil, 7,   'A', 1,   nil, 8,   3,   nil, nil, nil, 'C', nil,
-    nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, 'D', 6,   nil, 8,   nil, nil,
+    nil, nil, nil, nil, nil, 9, "B", nil, nil, 2, nil, nil, "D", 5, "A", nil,
+    9, 1, nil, 4, nil, nil, 3, nil, "A", nil, 7, nil, nil, nil, "C", nil,
+    "B", nil, nil, 2, nil, nil, "A", nil, nil, 9, nil, nil, nil, nil, 3, nil,
+    "C", nil, nil, nil, "D", nil, nil, 1, 8, nil, "B", "F", nil, 4, 6, nil,
+    nil, nil, nil, nil, "B", "D", nil, "G", 3, nil, 6, "A", 7, nil, nil, nil,
+    nil, "F", nil, "B", 8, "C", nil, nil, "D", "G", 9, 7, nil, nil, nil, 1,
+    3, nil, "A", nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, 2, 4, 6,
+    6, 7, nil, 5, 4, 3, nil, nil, nil, nil, nil, nil, 8, nil, nil, nil,
+    nil, nil, nil, 8, 7, nil, nil, nil, nil, nil, "A", 9, "F", nil, 1, 3,
+    "G", 2, 3, nil, nil, nil, nil, nil, nil, nil, 5, nil, nil, "C", nil, "A",
+    4, nil, nil, nil, 3, "G", 1, "F", nil, nil, 2, 8, 9, nil, 5, nil,
+    nil, nil, nil, 1, 5, "A", nil, 6, 4, nil, 3, "G", "B", nil, nil, nil,
+    nil, 6, "B", nil, 2, 1, nil, 3, 5, nil, nil, 4, nil, nil, nil, "F",
+    nil, 3, nil, nil, nil, nil, 9, nil, nil, 1, nil, nil, 5, nil, nil, 4,
+    nil, 5, nil, nil, nil, "F", nil, 4, nil, "B", nil, nil, 3, 8, 2, "C",
+    nil, 4, 2, "F", nil, nil, "D", "B", nil, 7, 8, nil, nil, nil, nil, nil,
+
   ]
+
+  #[
+  #  nil, nil, 'C', nil, 9,   'B', nil, nil, nil, nil, nil, nil, nil, nil, nil, nil,
+  #  nil, 2,   nil, nil, nil, 'D', 'G', nil, 7,   'B', 6,   nil, nil, 1,   nil, 'C',
+  #  nil, 1,   nil, 3,   nil, 'A', nil, 2,   'G', nil, nil, nil, nil, nil, nil, nil,
+  #  nil, nil, nil, 'D', 1,   3,   'F', nil, 'A', nil, nil, nil, nil, nil, nil, nil,
+  #  5,   6,   nil, nil, 3,   nil, nil, 9,   'F', nil, nil, nil, 4,   nil, nil, nil,
+  #  'B', nil, 9,   4,   nil, nil, 7,   'C', nil, 2,   nil, nil, nil, 'A', 1,   nil,
+  #  'D', nil, nil, nil, 'G', nil, 1,   nil, 5,   nil, nil, 8,   nil, nil, nil, 'E',
+  #  'E', nil, 'F', 2,   nil, nil, 5,   'A', nil, nil, nil, 'G', 'B', nil, nil, nil,
+  #  nil, nil, nil, 8,   'B', nil, nil, nil, 3,   4,   nil, nil, 'A', 'C', nil, 'G',
+  #  'G', nil, nil, nil, 'E', nil, nil, 5,   nil, 9,   nil, 'A', nil, nil, nil, 7,
+  #  nil, 4,   3,   nil, nil, nil, 9,   nil, 'E', 'D', nil, nil, 'F', 2,   nil, 'B',
+  #  nil, nil, nil, 'E', nil, nil, nil, 3,   8,   nil, nil, 'F', nil, nil, 6,   5,
+  #  nil, nil, nil, nil, nil, nil, nil, 'G', nil, 5,   2,   'E', 6,   nil, nil, nil,
+  #  nil, nil, nil, nil, nil, nil, nil, 'F', 4,   nil, 'G', nil, 'D', nil, 2,   nil,
+  #  'F', nil, 2,   nil, nil, 7,   'A', 1,   nil, 8,   3,   nil, nil, nil, 'C', nil,
+  #  nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, 'D', 6,   nil, 8,   nil, nil,
+  #]
 ]
 
-
-
-
 Benchmark.bm do |bm|
-  pzls.take(6).each_with_index do |ss, n|
+  pzls.reverse.take(1).each_with_index do |ss, n|
     bm.report do
       puts
       puts "-- puzzle #{n}"
