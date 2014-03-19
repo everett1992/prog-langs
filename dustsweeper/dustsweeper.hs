acd2dd4d2e95bb511c77ee89cfcddfa2179c16ae
Caleb Everett <everett.caleb@gmail.com>
2014-03-18 23:42:27 -0400

    Haskell and stuff.

--- dustsweeper/dustsweeper.hs
+++ dustsweeper/dustsweeper.hs
@@ -16,45 +16,54 @@ import Text.Read
 import Data.Maybe
 import Data.List
 
-data Rug = Rug { is_explored :: Bool, is_dust :: Bool }
+data Rug = Rug { is_explored :: Bool, dusts :: Int }
 
 type Board = [[Rug]]
 
 
 main = do
   args <- getArgs
-  case parseArgs args of
-    Left a  -> exitError a
-    Right a -> printBoard $ board (fst a) (snd a)
+  case parse_args args of
+    Left a  -> exit_error a
+    Right a -> print_board $ board (fst a) (snd a)
 
-rugChar rug
-  | (is_explored rug) && (is_dust rug)      = 'X'
-  | (is_explored rug) && (not $ is_dust rug)  = ' '
-  | not $ is_explored rug                     = '_'
+is_dust :: Rug -> Bool
+is_dust rug
+  | dusts rug == -1 = True
+  | otherwise = False
 
-printBoard board =
-    putStrLn $ unlines $ map (\row -> map rugChar row) board
+-- Converts a Rug to a char
+rug_char :: Rug -> Char
+rug_char rug
+  | (is_explored rug) = case (dusts rug) of num
+                                              | num == (-1)       -> 'X'
+                                              | num == 0           -> ' '
+                                              | otherwise   -> head $ show num
+  | otherwise = '░'
+
+print_board board =
+    putStrLn $ unlines $ map (\row -> map rug_char row) board
 
 board :: Int -> Int -> Board
-board size num_bombs = replicate size $ replicate size $ Rug False False
+board size num_dusts = replicate size $ replicate size $ Rug False 0
 
--- Returns Either a tuple of (size, num_bombs) or an error string.
-parseArgs :: [String] -> Either String (Int,Int)
-parseArgs args
+-- Returns Either a tuple of (size, num_dusts) or an error string.
+parse_args :: [String] -> Either String (Int,Int)
+parse_args args
   | length args /= 2 = Left $ "Wrong number of args: " ++ show (length args) ++ " of 2"
   | otherwise = case (readMaybe $ args !! 0, readMaybe $ args !! 1) of
     (Nothing,_)      -> Left $ "Invalid Size was " ++ args !! 0 ++ " needs Int."
-    (_,Nothing)      -> Left $ "Invalid number of bombs was "  ++ args !! 1 ++ " needs Int."
+    (_,Nothing)      -> Left $ "Invalid number of dusts was "  ++ args !! 1 ++ " needs Int."
     (Just a, Just b) -> Right (a,b)
 
 -- Print basic usage instructions
-putUsage = do
+put_usage = do
   progName <- getProgName
-  putStrLn $ "Usage: " ++ progName ++ " size:Int num_bombs:Int"
+  putStrLn $ "Usage: " ++ progName ++ " size:Int num_dusts:Int"
 
 -- Print error msg, usage, and exit with a nonzero exit status
-exitError msg = do
+exit_error msg = do
   putStrLn msg
-  putUsage
+  put_usage
   exitFailure
 
