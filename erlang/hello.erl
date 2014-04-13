91fb8367a526c89903074c2406527eb989343c1d
Caleb Everett <everett.caleb@gmail.com>
2014-04-13 14:31:56 -0400

    Start write up.

--- erlang/hello.erl
+++
@@ -1,5 +0,0 @@
--module(hello).
--export([hello_world/0,hello_world/1]).
-
-hello_world() -> io:format("Hello World~n").
-hello_world(Name) -> io:format("Hello ~s~n", [Name]).
---
+++ erlang/src/recursion.erl
@@ -0,0 +1,16 @@
+-module(recursion).
+-export([pascal/1]).
+
+
+pascal_row(1) -> [1];
+pascal_row(N) ->
+  Prev = pascal_row(N - 1),
+  lists:map(fun({A,B}) -> A + B end, lists:zip([0] ++ Prev, Prev ++ [0])).
+
+row_to_string(R) ->
+  String_list = lists:map(fun(I) -> integer_to_list(I) end, R),
+  string:join(String_list, " " ).
+
+pascal(N) ->
+  Rows = lists:map( fun(R) -> pascal_row(R) end, lists:seq(1, N)),
+  string:join(lists:map(fun(R) -> row_to_string(R) end ,Rows), "\n").
